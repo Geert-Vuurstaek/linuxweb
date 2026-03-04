5dd0b28b1d4cdc27e2909758e499d2de8c5e818b
Geert Vuurstaek <r0982632@student.thomasmore.be>
2026-03-04 22:27:40 +0100

    fix: Flannel --iface=enp0s8, Prometheus DNS targets, remove nodeSelector

--- vagrant/init-master.sh
+++ vagrant/init-master.sh
@@ -19,6 +19,10 @@ if [ "$(hostname)" = "k8s-master" ]; then
   # Install Flannel CNI
   su - vagrant -c "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
 
+  # Fix Flannel to use host-only interface (enp0s8) instead of NAT (enp0s3)
+  # Without this, all nodes appear as 10.0.2.15 and VXLAN overlay fails
+  su - vagrant -c "kubectl patch daemonset kube-flannel-ds -n kube-flannel --type=json --patch='[{\"op\":\"replace\",\"path\":\"/spec/template/spec/containers/0/args\",\"value\":[\"--ip-masq\",\"--kube-subnet-mgr\",\"--iface=enp0s8\"]}]'"
+
   # Save join command for workers
   kubeadm token create --print-join-command > /vagrant/join-command.sh
   chmod +x /vagrant/join-command.sh
